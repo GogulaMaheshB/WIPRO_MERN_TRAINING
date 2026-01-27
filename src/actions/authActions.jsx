d3b95431e58fc0ecbfaaa56257a73a2e756738ea
Gogula Mahesh <gogulamaheshb@gmail.com>
2026-01-27 18:09:53 +0530

    Remove misplaced Phase 4 files from repository root

--- src/actions/authActions.jsx
+++
@@ -1,9 +0,0 @@
-import AuthStore from "../stores/authStore";
-
-export const login = (user, pass) => {
-  AuthStore.login(user, pass);
-};
-
-export const logout = () => {
-  AuthStore.logout();
-};
--- src/actions/cartActions.jsx
+++
@@ -1,9 +0,0 @@
-import CartStore from "../stores/cartStore";
-
-export const addToCart = (food) => {
-  CartStore.add(food);
-};
-
-export const removeFromCart = (id) => {
-  CartStore.remove(id);
-};
